8a7ccd55dfdd588bc86ca9f8c343ae043f46f057
Vintall <eg.ilyashenko@gmail.com>
2020-11-21 22:40:23 +0200

    добавил движение персонажа, в зависимости от камеры. скрипты на движение, поворот камеры. движение на основе физики rigidbody

--- Assets/Internal Accets/Player/Cam_Movement.cs
+++ Assets/Internal Accets/Player/Cam_Movement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Cam_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject player;
+    [SerializeField] float y_angle = 60;
+    [SerializeField] float distance = 10;
+    [SerializeField] float rotation_power = 1f;
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    float r_axis;
+    void CheckRotate()
     {
-        
+        r_axis = Input.GetAxis("Cam_Rotation");
+        if (r_axis != 0)
+        {
+            RotateCam(r_axis);
+        }
+    }
+    public void MoveCam()
+    {
+        Vector3 forw = transform.forward;
+        transform.position = player.transform.position - forw * distance;
+    }
+    void RotateCam(float r)
+    {
+        r *= -1 * rotation_power / 10;
+        transform.RotateAround(player.transform.position, Vector3.up, r);
+    }
+    private void Update()
+    {
+        CheckRotate();
+        MoveCam();
     }
 }
--- Assets/Internal Accets/Player/Player_Movenent.cs
+++ Assets/Internal Accets/Player/Player_Movenent.cs
@@ -4,24 +4,69 @@ using UnityEngine;
 
 public class Player_Movenent : MonoBehaviour
 {
-    Vector3 speed;
+
+    [SerializeField] float move_speed = 10f;
+    [SerializeField] float jump_speed = 10f;
+    [SerializeField] Camera cam;
     Rigidbody rb;
+    Vector3 start_angle;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        movement = new Vector3(0, 0, 0);
+        start_angle = transform.rotation.eulerAngles;
     }
-    Vector3 movement;
-    // Update is called once per frame
-    void Update()
+    bool is_on_ground = false;
+    void OnGroundCheck(bool is_on, Collision collision)
     {
-        movement.x = Input.GetAxis("Horizontal");
-        movement.z = Input.GetAxis("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            movement.y = 5;
-        }
-        speed = movement;
-        rb.AddForce(speed);
+        if (collision.gameObject.tag == "Ground")
+            is_on_ground = is_on;
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        OnGroundCheck(true, collision);
+        Debug.Log("On Ground");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        OnGroundCheck(false, collision);
+        Debug.Log("In air space");
+    }
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        MoveLogic();
+        if (is_on_ground)
+            JumpLogic();
+    }
+    void MoveLogic()
+    {
+        float vertical_axis = Input.GetAxis("Vertical");
+        float horizontal_axis = Input.GetAxis("Horizontal");
+
+        Vector3 cam_forv = cam.transform.forward.normalized;
+        Vector3 axis = new Vector3(vertical_axis, 0, horizontal_axis);
+        float angle = Vector3.Angle(cam_forv, axis);
+        cam_forv.y = 0;
+        int horizontal_abs;
+        if (horizontal_axis == 0) horizontal_abs = 0;
+        else horizontal_abs = (int)(horizontal_axis / Mathf.Abs(horizontal_axis));
+
+        Vector3 movement_vertical = cam_forv * vertical_axis;
+        Vector3 movement_horizontal = new Vector3(cam_forv.z * horizontal_abs, 0, -1 * cam_forv.x * horizontal_abs);
+        Vector3 movement = movement_horizontal + movement_vertical;
+
+        rb.AddForce(movement.normalized * move_speed, ForceMode.Impulse);
+    }
+    void JumpLogic()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rb.AddForce(Vector3.up * jump_speed);
+        }
+    }
+
 }
